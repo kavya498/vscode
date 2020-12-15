c453a34fd81de600d0a180249c4fe28ff618b1c3
kavya498 <46739084+kavya498@users.noreply.github.com>
2020-12-15 17:43:07 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ic_api_key{
+  description = "IBMCloud API Key"
+  type        = string
+}
+
+variable "region" {
+  description = "Region in which resource has to be provisioned."
+  type        = string
+  default     = "us-south"
+}
+variable "cms_name" {
+  type        = string
+  description = "CMS Service instance name"
+}
